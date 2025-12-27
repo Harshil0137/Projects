2e2768b0b20809ed2b0f3166f2e53c5ae1086a19
Harshil0137 <2403051050718@paruluniversity.ac.in>
2025-12-27 09:00:25 +0530

    Update RockPaperScissorsGame.java

--- RockPaperScissorsGame.java
+++ RockPaperScissorsGame.java
@@ -3,16 +3,21 @@ class Main{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         char p1,p2;
+        int a=0,b=0;
+        for(int i=0;i<3;i++){
         p1 = sc.next().charAt(0);
         p2 = sc.next().charAt(0);
-        if(p1=='R'&&p2=='P') System.out.println("p2 wins");
-        else if(p1=='P'&&p2=='R') System.out.println("p1 wins");
-        else if(p1=='P'&&p2=='S') System.out.println("p2 wins");
-        else if(p1=='S'&&p2=='P') System.out.println("p1 wins");
-        else if(p1=='S'&&p2=='R') System.out.println("p2 wins");
-        else if(p1=='R'&&p2=='S') System.out.println("p1 wins");
-        else if(p1==p2) System.out.println("Tie");
+        if(p1=='R'&&p2=='P') b++;
+        else if(p1=='P'&&p2=='R') a++;
+        else if(p1=='P'&&p2=='S') b++;
+        else if(p1=='S'&&p2=='P') a++;
+        else if(p1=='S'&&p2=='R') b++;
+        else if(p1=='R'&&p2=='S') a++;
         else System.out.println("Invalid Input");
         
     }
+    if(a>b) System.out.println("P1");
+    if(a<b) System.out.println("P2");
+    if(a==b) System.out.println("Tie");
+}
 }
